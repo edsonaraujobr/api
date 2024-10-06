d12dfc902d1f43c58df18d1bd2f9a308086cb258
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2024-10-06 15:40:50 -0300

    raw: add middleware to routes on administrator

--- src/routes/administrators.routes.js
+++ src/routes/administrators.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { loginAdministrator, createAdministrator, updateAdministrator, deleteAdministrator } from "../controllers/administrators.controllers.js";
+import { authorizeAdmistrator } from "../middlewares/authUser.middleware.js";
 
 const router = express.Router();
 
 router.post("/administrator/login", loginAdministrator);
 router.post("/administrator/create", createAdministrator);
-router.post("/administrator/update/:id", updateAdministrator);
-router.post("/administrator/delete/:id", deleteAdministrator);
+router.post("/administrator/update/:id", authorizeAdmistrator, updateAdministrator);
+router.post("/administrator/delete/:id", authorizeAdmistrator, deleteAdministrator);
 
 export default router;
